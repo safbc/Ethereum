00db851a31f5ac111eb468d027c5bf2dd7b53415
Coenie <coeniebeyers@gmail.com>
2016-10-28 11:51:21 +0200

    Account functions are now moved out of the tests.

--- test/accountManagement.js
+++ test/accountManagement.js
@@ -1,12 +1,12 @@
 var expect = require('expect.js');
 var async = require('async');
 
-var keythereum = require("keythereum");
-var Tx = require('ethereumjs-tx');
 var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:20000'));
 //web3.setProvider(TestRPC.provider());
+
+var accountManagement = require('../AccountManagement/accountManagement.js');
 
 var newBlockEvents = require('../Events/newBlockEvents.js');
 var events = require('../Events/eventEmitter.js');
@@ -17,27 +17,23 @@ describe('Account Management:', function() {
   this.timeout(60 * 1000);
   newBlockEvents.Start();
   it('should be able to create a public/private key pair', function(done) {
-    var account = {};
-    account.privateKey = keythereum.create().privateKey.toString('hex');
-    account.address = keythereum.privateKeyToAddress(account.privateKey);
+    var account = accountManagement.NewAccount();
     accounts.push(account);
-    expect(account.privateKey).to.not.be(null);
-    expect(account.privateKey).to.be(accounts[0].privateKey);
-    expect(account.address).to.not.be(null);
-    expect(account.address).to.be(accounts[0].address);
+    expect(account).to.not.be(null);
+    expect(accountManagement.ListAccounts.length).to.be.greaterThan(0);
     done();
   });
   it('the new account should be able to receive some funds', function(done){
     web3.personal.unlockAccount(web3.eth.coinbase, '1234', function(err, res){  
       var tx = {
         from: web3.eth.coinbase, 
-        to: accounts[0].address, 
+        to: accounts[0],
         value: 2000000
       };
       web3.eth.sendTransaction(tx, function(err, res){
         if(err){console.log('ERROR:', err);}
         events.once('newBlock', function(block, intent){
-          web3.eth.getBalance(accounts[0].address, function(err, balance){
+          web3.eth.getBalance(accounts[0], function(err, balance){
             if(err) {console.log('ERROR:', err);}
             expect(balance.c[0]).to.be(parseInt(tx.value, 16)); 
             done();
@@ -47,29 +43,25 @@ describe('Account Management:', function() {
     });
   });
   it('the new account should be able to send some funds', function(done){
-    var account = {};
-    account.privateKey = keythereum.create().privateKey.toString('hex');
-    account.address = keythereum.privateKeyToAddress(account.privateKey);
+    var account = accountManagement.NewAccount();
     accounts.push(account);
 
     var rawTx = {
-      from: accounts[0].address, 
-      to: accounts[1].address, 
+      from: accounts[0], 
+      to: accounts[1], 
       gasPrice: '0x'+padToEven(Number(1).toString(16)),
-      gasLimit: '0x'+padToEven(Number(100000).toString(16)),
+      gasLimit: '0x'+padToEven(Number(25000).toString(16)),
       value: 1000
     };
-
-    web3.eth.getTransactionCount(accounts[0].address, function(err, nonce){
-      rawTx.nonce = '0x'+nonce.toString(16);
-      var tx = new Tx(rawTx);
-      var privateKey = new Buffer(accounts[0].privateKey, 'hex');
-      tx.sign(privateKey);
-      var serializedTx = tx.serialize();
-      web3.eth.sendRawTransaction(serializedTx.toString('hex'), function(err, hash) {
+    accountManagement.SignRawTransaction(rawTx, accounts[0], function(tx){
+      console.log('1');
+      web3.eth.sendRawTransaction(tx, function(err, hash) {
+        console.log('2');
         if (err) {console.log('ERROR:', err);}
         events.once('newBlock', function(block, intent){
-          web3.eth.getBalance(accounts[1].address, function(err, balance){
+          console.log('3');
+          web3.eth.getBalance(accounts[1], function(err, balance){
+            console.log('4');
             if(err) {console.log('ERROR:', err);}
             expect(balance.c[0]).to.be(rawTx.value); 
             done();
