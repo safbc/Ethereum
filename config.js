3c2ac4df4917f46f0590fa2af246018d621ff7a6
Coenie <coeniebeyers@gmail.com>
2016-11-01 11:07:03 +0200

    Adding config gile

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.dbConnectionString = 'springblock';
+config.dbCollections = ['userRegistry'];
+
+config.zarIssuerUsername = 'zarissuer';
+
+config.rpcaddress = 'http://localhost:20000';
+
+module.exports = config;
+
